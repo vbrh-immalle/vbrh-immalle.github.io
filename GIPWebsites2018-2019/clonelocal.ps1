514ee790a4141932f7560221a029bcd99fd20eb1
Marcin Kostulski <31651297+MarcinK-immalle@users.noreply.github.com>
2019-02-15 14:19:41 +0100

    Add_Marcin

--- GIPWebsites2018-2019/clonelocal.ps1
+++ GIPWebsites2018-2019/clonelocal.ps1
@@ -1,7 +1,7 @@
 $repos = @(
     @{githubName="vbrh-immalle"; repoUrl="https://github.com/vbrh-immalle/vbrh-immalle.github.io"}
     # TODO: voeg je eigen site toe:
-    #@{githubName="vbrh-immalle"; repoUrl="https://github.com/vbrh-immalle/vbrh-immalle.github.io"},
+    @{githubName="MarcinK-immalle";},
     
 )
 
